02a7d6bc52463111b3cac0ca0d6031dd57cb164d
patsusanto <susantopatrick012@gmail.com>
2024-08-18 20:46:56 +0200

    created first form elements for task page

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,15 +1,42 @@
-import { Container, Flex, Heading } from "@radix-ui/themes";
-
+import { Container, Flex, Heading, RadioGroup, TextArea, TextField } from "@radix-ui/themes";
+import * as Label from "@radix-ui/react-label";
 function TaskForm() {
     return (
-        <Container>
+        <Container p="5">
             <Heading>Manage my tasks</Heading>
-            <Flex>
+            <Flex direction="row">
+                <Flex direction="row" gap="3" className="task-form rounded-xl" p="5" mt="5">
+                    <form>
+                        <Flex direction="column" px="5">
+                            <Label.Root className="label text-[15px] font-medium leading-[35px]" htmlFor="firstName">
+                                Task Name
+                            </Label.Root>
+                            <TextField.Root placeholder="enter your task" />
+                        </Flex>
+                        
+                        <Flex p="5">
+                            <RadioGroup.Root color="lime" defaultValue="1" name="example">
+                                <RadioGroup.Item className="label" value="1">Default</RadioGroup.Item>
+                                <RadioGroup.Item className="label" value="2">Comfortable</RadioGroup.Item>
+                                <RadioGroup.Item className="label" value="3">Compact</RadioGroup.Item>
+                            </RadioGroup.Root>
+                        </Flex>
+                        
+                        <Flex direction="column" px="5">
+                            <Label.Root className="label text-[15px] font-medium leading-[35px]" htmlFor="firstName">
+                                Details
+                            </Label.Root>
+                            <TextArea placeholder="Enter the details.." />
+                        </Flex>
+                    </form>
+                </Flex>
 
+                <Flex>
+        
+                </Flex>
             </Flex>
-
-
         </Container>
+        
     )
     
 }
